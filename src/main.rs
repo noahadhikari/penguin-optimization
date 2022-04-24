795e6ed8a61308d9029ad1bae73ff65d1e2f476e
meshankhosla <meshan02@gmail.com>
2022-04-23 21:18:05 -0700

    Add stopwatch

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ mod grid;
 mod lp;
 use grid::Grid;
 
+use std::fmt::Error;
 use std::fs;
+use std::fs::DirEntry;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::Write;
 use std::io::{self, BufReader};
+use stopwatch::Stopwatch;
 
 fn solve_all_inputs() {
     const CUTOFF_TIME: u32 = 500000; //max time in seconds
@@ -44,39 +47,72 @@ fn solve_one_input() {
     println!("{}", grid);
 }
 
-fn solve_one_randomized() {
-    const INPUT_PATH: &str = "./inputs/test/small.in";
-    const OUTPUT_PATH: &str = "./outputs/test/small_rand.out";
+fn solve_all_randomized() {
+	let paths = fs::read_dir("./inputs/medium").unwrap();
+
+	// Will find a better way for this
+	let mut i = 1;
+	for path in paths {
+
+		// There's probably a much better way to do this
+		match i {
+			1..=10 => {
+				// Uncomment for 11-20
+				// i += 1;
+				// continue
+			}, 
+			11..=20 => {
+				// Comment out for 11-20
+				continue;
+			}
+			_ => return,
+		}
+		
+		let real_path = path.unwrap().path();
+		let test_number = real_path.file_stem().unwrap().to_str().unwrap(); // ie: 001
+		let input_path = real_path.to_str().unwrap();
+		let output_path = "./outputs/".to_string() + "medium/" + test_number + ".out";
+		solve_one_randomized(input_path, &output_path, 300);
+		
+		i += 1;
+	}
+
+}
+
+fn solve_one_randomized(input_path: &str, output_path: &str, secs_per_input: u64) {
+    // const INPUT_PATH: &str = "./inputs/medium/001.in";
+    // const OUTPUT_PATH: &str = "./outputs/medium/001.out";
     const CUTOFF_TIME: u32 = 60; //max time in seconds
     const ITERATIONS: u32 = 10000;
 
     let mut best_penalty_so_far = f64::INFINITY;
     let mut best_grid_so_far = Grid::new(0, 0, 0);
-    for i in 0..ITERATIONS {
-        let mut grid = get_grid(INPUT_PATH).unwrap();
-        let p = grid.random_lp_solve(CUTOFF_TIME);
-        println!("{} penalty: {}", i, p);
-        if p < best_penalty_so_far {
-            best_penalty_so_far = best_penalty_so_far.min(p);
-            best_grid_so_far = grid;
-        }
+		let sw = Stopwatch::start_new();
+		// For every file:			
+		while sw.elapsed().as_secs() < secs_per_input { // 5 mins
+			let mut grid = get_grid(input_path).unwrap(); // Need a way to move this out
+			let p = grid.random_lp_solve(CUTOFF_TIME);
+			// println!("{} penalty: {}", i, p);
+			if p < best_penalty_so_far {
+					best_penalty_so_far = best_penalty_so_far.min(p);
+					best_grid_so_far = grid;
+			}
 
-        // Print out best_so_far every 50
-        if i % 50 == 0 {
-            println!("Best so far: {}", best_penalty_so_far);
-        }
-    }
+			let time = sw.elapsed().as_secs();
+			if sw.elapsed().as_secs() % 10 == 0 {
+				println!("{} secs passed. Best so far: {}", time, best_penalty_so_far);
+			}
+		}
     println!("Best: {}", best_penalty_so_far);
-    // write_sol(&best_grid_so_far, OUTPUT_PATH);
-
-    // println!("Valid: {}", best_grid_so_far.is_valid());
-    // println!("{}", grid);
+    println!("Valid: {}", best_grid_so_far.is_valid());
+    write_sol(&best_grid_so_far, output_path);
 }
 
 fn main() {
     // solve_all_inputs();
     // solve_one_input();
-    solve_one_randomized();
+    // solve_one_randomized();
+		solve_all_randomized();
 }
 
 // Algorithms
